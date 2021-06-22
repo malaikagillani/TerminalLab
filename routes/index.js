7303f30b74005c6a8fb2c427b6736fdb3097e28a
malaikagillani <FA18-BSE-029@cuilahore.edu.pk>
2021-06-22 20:56:03 +0500

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/shop', function(req, res, next) {
+  let shop= req.cookies.shop;
+  if(!shop) shop=[];
+  res.render("shop",{shop});
+});
 module.exports = router;
